b745c34d4936facef4bdfec0047fb3e4386c905a
Kenken64 <bunnyppl@hotmail.com>
2018-04-16 10:57:03 +0800

    Update firebase.config.ts

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyCUquO7_GRorg1n11aOi3_9G4FgRt1woq0",
-    authDomain: "fsf2018r1.firebaseapp.com",
-    databaseURL: "https://fsf2018r1.firebaseio.com",
-    projectId: 'fsf2018r1',
-    storageBucket: "fsf2018r1.appspot.com",
-    messagingSenderId: "962119018181"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: '',
+    storageBucket: "",
+    messagingSenderId: ""
 };
