50f3c8e49a1caf939170d9e4b7ec4655c8d4ce8c
Kenneth Phang <bunnyppl@gmail.com>
2018-05-23 17:02:21 +0800

    fix: latest sql

--- db/latest_tables.sql
+++ db/latest_tables.sql
@@ -7,5 +7,30 @@ CREATE TABLE `books` (
   `modified_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `email` varchar(450) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=126 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=128 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `gallery` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(200) DEFAULT NULL,
+  `fileUrl` varchar(300) DEFAULT NULL,
+  `remarks` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `bio` text,
+  `hash` text,
+  `salt` varchar(200) DEFAULT NULL,
+  `imageurl` varchar(400) DEFAULT NULL,
+  `isDelete` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+SELECT * FROM library_db.books;
